fb2bab0d0e1b0cc9a5d4e530ebfce2e6765f4423
dneri95 <160014456+dneri95@users.noreply.github.com>
2024-02-24 22:13:48 -0800

    Update cis129_lab03_coffeeShop.py
    changed format of receipt.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -9,7 +9,7 @@ muffin = int(input(' Number of muffins bought?\n '))
 fruits = int(input(' Number of fruits bought?\n '))
 smoothies = int(input(' Number of smoothies bought?\n '))
 
-#price of total cost of products individually
+#price of total cost of products individaly
 coffee_total = coffee * 5.00
 muffin_total = muffin * 4.00
 fruits_total = fruits * 2.00
@@ -22,10 +22,16 @@ tax = (coffee_total + muffin_total + fruits_total + smoothies_total) * .06
 total = coffee_total + muffin_total + fruits_total + smoothies_total + tax
 
 #print receipt
-print(' ***************************************\n\n ***************************************\n My Coffee and Muffin Shop Receipt\n',
-      coffee, 'Coffee at $5 each: $', coffee_total, '\n' ,muffin, 'Muffin at $4 each: $',
-      muffin_total, '\n', fruits, 'Fruits at $2 each: $', fruits_total, '\n', smoothies, 'Smoothies at $6 each: $', smoothies_total, 
-      '\n', '6% tax: $', tax, '\n', '---------\n', 'Total: $', total, '\n', '***************************************')
+print(' ***************************************\n\n ***************************************\n'
+         '   My Coffee and Muffin Shop Receipt\n',
+         coffee,     'Coffee at $5 each:     $',       coffee_total, '\n',
+         muffin,     'Muffin at $4 each:     $',       muffin_total, '\n',
+         fruits,     'Fruits at $2 each:     $',       fruits_total, '\n', 
+         smoothies,  'Smoothies at $6 each:  $',       smoothies_total, '\n',
+        '6% tax:                  $',                   tax, '\n ',
+        '                       ---------\n', 
+        'Total:                   $',                  total, '\n', 
+       '***************************************')
 
 #message to customer
 print(' Dont have a good day, Have a great day!')
